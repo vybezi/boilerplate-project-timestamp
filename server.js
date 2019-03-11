29cd6624128f7f1099002109704a242250488ecd
John Gordon <Jagordon09@gmail.com>
2019-03-11 11:42:39 -0500

    Update server.js

--- server.js
+++ server.js
@@ -24,6 +24,36 @@ app.get("/api/hello", function (req, res) {
   res.json({greeting: 'hello API'});
 });
 
+app.get('/api/timestamp/:date_string?', function(req,res){
+    let now = new Date();
+    let date_string;
+    let response = {};
+
+    if(!req.params.date_string){
+        res.send({unix:now.getTime(),utc:now.toUTCString()});
+        return;
+    }
+
+    date_string = req.params.date_string;
+
+    if(parseFloat(date_string) && (date_string != "0" || date_string == 0)){
+        date_string = parseFloat(date_string);
+        response.unix = new Date(date_string).getTime();
+        response.utc = new Date(date_string).toUTCString();
+    }else if(new Date(date_string) != "Invalid Date"){
+        response.unix = new Date(date_string).getTime();
+        response.utc = new Date(date_string).toUTCString();
+    }else if(date_string == "0" || date_string == 0){
+        response.unix = new Date(0).getTime();
+        response.utc = new Date(0).toUTCString();
+    }else{
+        return {error:"Invalid Date"};
+    }
+
+
+    res.send(response);
+})
+
 
 
 // listen for requests :)
